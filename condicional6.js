22881b1c1c3ee09fae0dafb5a03357c6dfe6d76c
lui1177 <160692857+lui1177@users.noreply.github.com>
2024-03-15 15:28:02 -0500

    Update condicional6.js

--- condicional6.js
+++ condicional6.js
@@ -1,19 +1,19 @@
 console.log("Escriba dos numeros")
 
-let A = -20
-let B = 2
+let a = -20
+let b = 2
 
-console.log(A)
-console.log(B)
+console.log(a)
+console.log(b)
 
-if(  A < B  ){
-    let resultado = (-A) + (+B)
+if(  a < b  ){
+    let resultado = (-a) + (+b)
     console.log("El resultado es: ")
     console.log(resultado)
 
 }else{
-    if(A > B ){
-    let resultado = A * B
+    if(a > b ){
+    let resultado = a * b
     console.log("El resultado es: ")
     console.log(resultado)
  }
