bf5b8e4c4a744639ff877d8dee5bba34d9a3e3fe
lui1177 <160692857+lui1177@users.noreply.github.com>
2024-03-15 15:45:36 -0500

    Update condicional5.js

--- condicional5.js
+++ condicional5.js
@@ -2,20 +2,19 @@ console.log("Ingrese dos numeros")
 
 console.log("Recuerde que la divison no puede ser 0")
 
-let a = 1000
-let b = 0
-
+let n = 1000
+let d = 0
 
 console.log(a)
 console.log(b)
 
-console.log("El dividendo es: "+a)
-console.log("El divisor es: "+b)
+console.log("El dividendo es: "+n)
+console.log("El divisor es: "+d)
 
-let cociente = a / b
+let cociente = n/d
 console.log("El cociente es: "+cociente)
-if (a || b == 0){
-    let dividir = a / b
+if (n || d == 0){
+    let dividir = n/d
     console.log("La divison no es posible")
 }
 
